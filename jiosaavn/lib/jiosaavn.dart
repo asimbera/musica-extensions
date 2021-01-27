be056fa3fec2028114aab2cdf604fd5990a51d5c
Asim Bera <asimbera@outlook.in>
2021-01-27 20:13:08 +0530

    reinitialize extensions

---
+++ jiosaavn/lib/jiosaavn.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library jiosaavn;
+
+export 'src/jiosaavn_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ wynk/example/wynk_example.dart
@@ -0,0 +1,6 @@
+import 'package:wynk/wynk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
